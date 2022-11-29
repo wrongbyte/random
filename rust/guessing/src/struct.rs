1af9db08245432cc4652192bc23f93ede8323e56
by <57643375+wrongbyte@users.noreply.github.com>
2022-11-29 12:27:38 -0300

    traits

--- rust/guessing/src/struct.rs
+++ rust/guessing/src/struct.rs
@@ -4,13 +4,40 @@ struct Rectangle {
     width: u32,
 }
 
-fn main() {
-    let rect1 = Rectangle { length: 50, width: 30 };
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.length * self.width
+    }
 
-    println!("The area is: {}", area(&rect1));
-    println!("The rect is {:#?}", rect1);
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.length > other.length && self.width > other.width
+    }
+
+    fn square(size: u32) -> Self {
+        Rectangle {
+            length: size,
+            width: size,
+        }
+    }
 }
 
-fn area(rect: &Rectangle) -> u32 {
-    rect.length * rect.width
+fn main() {
+    let rect1 = Rectangle {
+        length: 50,
+        width: 30,
+    };
+    let rect2 = Rectangle {
+        length: 40,
+        width: 10,
+    };
+    let rect3 = Rectangle {
+        length: 45,
+        width: 60,
+    };
+
+    let sq1 = Rectangle::square(10);
+
+    println!("The area is: {}", rect1.area());
+    println!("The area is: {}", sq1.area());
+    println!("The rect is {:#?}", rect1);
 }
